2963806f9ffeb7b80fb697f22e3de4e76762564c
nidhiraviprasad <48962793+nidhiraviprasad@users.noreply.github.com>
2019-11-09 18:00:28 -0800

    quiz questions added!

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,114 @@
+var quizQuestions = [
+  {
+      question: "You enter an enchanted garden. What would you be most curious to examine first?",
+      answers: [
+          {
+              type: "Ravenclaw",
+              content: "The silver leafed tree bearing golden apples"
+              
+          },
+ 	  {
+	      type: "Hufflepuff",
+	      content: "The fat red toadstools that appear to be talking to each other"
+	  },
+          {
+              type: "Slytherin",
+              content: "The bubbling pool, in the depths of which something luminous is swirling"
+          },
+          {
+              type: "Gryffindor",
+              content: "The statue of an old wizard with a strangely twinkling eye"
+          }
+      ]
+  },
+  {
+      question: "Which road tempts you most?",
+      answers: [
+          {
+              type: "Ravenclaw",
+              content: "The cobbled street lined with ancient buildings"
+
+          },
+	      {
+	          type: "Hufflepuff",
+              content: "The wide, sunny, grassy lane"
+
+          },
+          {
+              type: "Slytherin",
+              content: "The narrow, dark, lantern-lit alley"
+          },
+          {
+              type: "Gryffindor",
+              content: "The twisting, leaf-strewn path through woods"
+          }
+      ]
+  },
+  {
+      question: "Four goblets are placed before you. Which would you choose to drink?",
+      answers: [
+          {
+              type: "Ravenclaw",
+              content: "The foaming, frothing, silvery liquid that sparkles as though containing ground diamonds."
+          },
+          {
+              type: "Hufflepuff",
+              content: "The smooth, thick, richly purple drink that gives off a delicious smell of chocolate and plums."
+          },
+          {
+              type: "Slytherin",
+              content: "The mysterious black liquid that gleams like ink, and gives off fumes that make you see strange visions."
+          },
+	      {
+              type: "Gryffindor",
+              content: "The golden liquid so bright that it hurts the eye, and which makes sunspots dance all around the room."
+          }
+
+      ]
+  },
+  {
+      question: "A Muggle confronts you and says that they are sure you are a witch or wizard. Do you:",
+      answers: [
+          {
+              type: "Ravenclaw",
+              content: "Ask what makes them think so"
+          },
+          {
+              type: "Hufflepuff",
+              content: "Tell them that you are worried about their mental health, and offer to call a doctor"
+          },
+          {
+              type: "Slytherin",
+              content: "Agree, and ask them whether they'd like a free sample of a jinx"
+          },
+
+	      {
+              type: "Gryffindor",
+              content: "Agree, and walk away, leaving them to wonder whether you are bluffing"
+          }
+      ]
+  },
+  {
+      question: "After you have died, what would you most like people to do when they hear your name?",
+      answers: [
+	      {
+              type: "Ravenclaw",
+              content: "Think with admiration of your achievements."
+          },
+          {
+              type: "Hufflepuff",
+              content: "Miss you, but smile."
+          },
+          {
+              type: "Slytherin",
+              content: "I don't care what people think of me after I'm dead; it's what they think of me when I'm alive that counts."
+          },
+          {
+              type: "Gryffindor",
+              content: "Ask for more stories about your adventures."
+          }
+      ]
+  }
+];
+ 
+export default quizQuestions;
